c8e84fcdba78ecae8ba7d537abc665f9dc159e92
sachin <sachin14051@gmail.com>
2024-01-03 20:13:59 +0530

    working on declarations

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,11 @@ let baseUrl = `https://jsonplaceholder.typicode.com/posts`;
 let photosUrl = `https://jsonplaceholder.typicode.com/photos`;
 let albumUrl = `https://jsonplaceholder.typicode.com/albums`;
 
+let x =10;
+
+
+
+
 
 
 const postTable = document.getElementById("post-table")
